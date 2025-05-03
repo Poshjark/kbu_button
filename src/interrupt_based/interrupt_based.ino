107b48b61fd8db05e5905a44c24f6dd96f079e3f
Poshjark <alximik328@yandex.ru>
2025-05-03 19:30:18 +0700

    Interrupt based version: * Finally stabilized(added comment about choosing right board) * Key is renamed to Button as in loopbased version * Interrupts are now just turning loop to the "button pressed" state. Loop is performing prints and so on to avoid delaying in interrupt handler

--- src/interrupt_based/interrupt_based.ino
+++ src/interrupt_based/interrupt_based.ino
@@ -1,13 +1,27 @@
+/*
+*     VERY IMPORTANT:
+*     COMPILE AND DEPLOY PROJECT WITH "Arduino Micro" BOARD CHOSEN
+*     OTHERWISE YOUR BOARD HID-FUNCTIONALITY CAN BE TURNED OFF BECAUSE OF USING
+*     HID-RESERVED PINS.
+*     Or just use right pin:)
+*
+*/
+
 #include "Keyboard.h"
 
-#define KEY_PIN 7
+#define BUTTON_PIN 7
 
-volatile unsigned long LastPrint{0};
+constexpr unsigned long long debounceDelay = 100ul;
 
-const uint8_t K_key = 82;
-const uint8_t B_key = 60;
-const uint8_t U_key = 69;
-const uint8_t QUEST_key = 38;
+volatile unsigned long long lastInterrupt{0};
+volatile bool buttonPressed {false};
+volatile uint8_t currentTypeStep{0};
+
+// empirically established
+const uint8_t K_key = 0x52;
+const uint8_t B_key = 0x3C;
+const uint8_t U_key = 0x45;
+const uint8_t QUEST_key = 0x26;
 
 
 constexpr auto KBU_PHRASE_LEN = 4ull;
@@ -16,48 +30,62 @@ const uint8_t KBU_PHRASE[KBU_PHRASE_LEN]
   K_key, B_key, U_key, QUEST_key
 };
 
-void PrintKBU()
+
+void InterruptHandler()
 {
+  // just software debounce
   const auto now = millis();
-  if((now - LastPrint) < 100)
+  if((now - lastInterrupt) < debounceDelay)
   {
     return;
   }
-  for(unsigned long long i = 0; i < KBU_PHRASE_LEN; ++i)
-  {
-      Keyboard.press(KBU_PHRASE[i]);
-      Keyboard.release(KBU_PHRASE[i]);
 
-      // Optional sleep. It's highly recommended not to "sleep" in interruptions
-      // but some usb drivers may skip some signals
-      //delay(5);
-  }
-
-  LastPrint = now;
+  lastInterrupt = now;
+  buttonPressed = true;
 }
+
 void setup() 
 {
   // initialize control over the keyboard:
   Keyboard.begin();
 
   // Initialize key pin mode
-  pinMode(KEY_PIN, INPUT);
+  pinMode(BUTTON_PIN, INPUT);
 
-  // Initializing last print timestamp for using in soft switch bounce fix
-  // There will be hard fix but we are paranoids...
-  LastPrint = 0;
+  // Initializing last print timestamp for using in software debounce
+  // The hardware debounce is implemented by external pullup resistor but we are paranoids...
+  lastInterrupt = 0;
 
+  // IMPORTANT(again): CHOOSE RIGHT BOARD 
   // Attaching function to hardware interruption
   // on LOW because:
-  // * When key is not pressed current flows to KEY_PIN and voltage there is HIGH(non-zero)
-  // * When key IS pressed current flows to the ground and voltage on KEY_PIN is LOW
+  // * When key is not pressed current flows to BUTTON_PIN and voltage there is HIGH(non-zero)
+  // * When key IS pressed current flows to the ground and voltage on BUTTON_PIN is LOW
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), InterruptHandler, LOW);
+}
 
-  attachInterrupt(0, PrintKBU, LOW);
+void PrintKey(uint8_t key)
+{
+  Keyboard.press(key);
+  Keyboard.release(key);
+}
 
+void TryPerformKBUprintStep()
+{
+  if(buttonPressed)
+  {
+    PrintKey(KBU_PHRASE[currentTypeStep]);
+
+    if(++currentTypeStep >= KBU_PHRASE_LEN)
+    {
+      currentTypeStep = 0;
+      buttonPressed = false;
+    }
+  }
 }
 
 void loop() 
 {
-  // It would be great to turn device into deep sleep here
-  // but there is no reason to do it because device will always be powered from the PC
+  TryPerformKBUprintStep();
+  delay(20);
 }
